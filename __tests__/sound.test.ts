967c267b715bcb714fd7c1bc18f16060818e20be
RaphaelBRodrigues <raphaelbarbosa.rodrigues@gmail.com>
2022-05-05 21:56:04 -0300

    initial config

---
+++ __tests__/sound.test.ts
@@ -0,0 +1,5 @@
+describe('Playing sound', () => {
+  it('should play tomou na jabiraca', () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ src/constants/filenames.ts
@@ -0,0 +1,1 @@
+export const JABIRACA_FILE_NAME = 'tomou_na_jabiraca.mp3';
