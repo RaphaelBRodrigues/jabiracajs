19bda77b50a5658145c80b7188c489c0845c9432
RaphaelBRodrigues <raphaelbarbosa.rodrigues@gmail.com>
2022-05-07 23:28:55 -0300

    chore: updating package version

--- src/constants/cli.ts
+++ src/constants/cli.ts
@@ -1,4 +1,4 @@
-export const CLI_VERSION = 'v1.0.6';
+export const CLI_VERSION = 'v1.0.7';
 export const CLI_DESCRIPTION =
   'This awesome cli will let you know when the things went wrong';
 export const CLI_NAME = 'jabiracajs';
