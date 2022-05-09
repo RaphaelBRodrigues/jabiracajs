be806a016b59f96bdf44926e284d0c6d68e54aba
RaphaelBRodrigues <raphaelbarbosa.rodrigues@gmail.com>
2022-05-09 18:59:16 -0300

    chore: adjusting cli version

--- src/constants/cli.ts
+++ src/constants/cli.ts
@@ -1,4 +1,6 @@
-export const CLI_VERSION = 'v1.0.7';
+export const CLI_VERSION = `v${process.env.npm_package_version}` || '1.0.8';
 export const CLI_DESCRIPTION =
   'This awesome cli will let you know when the things went wrong';
 export const CLI_NAME = 'jabiracajs';
+
+console.log(process.env.npm_package_version);
