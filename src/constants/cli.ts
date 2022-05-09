45fac2ad7154569feeef50692fa312b7b1cdbd1a
RaphaelBRodrigues <raphaelbarbosa.rodrigues@gmail.com>
2022-05-09 19:09:54 -0300

    chore: adding npmrc

--- src/constants/cli.ts
+++ src/constants/cli.ts
@@ -2,5 +2,3 @@ export const CLI_VERSION = `v${process.env.npm_package_version}` || '1.0.8';
 export const CLI_DESCRIPTION =
   'This awesome cli will let you know when the things went wrong';
 export const CLI_NAME = 'jabiracajs';
-
-console.log(process.env.npm_package_version);
